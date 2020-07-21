1319222aefa5dc92b2e07a54c3a3945069750b10
Bruno Santi Garcia <bruno.santi.98@outlook.com>
2020-07-20 22:25:09 -0300

    [+] Add Banner, Profile data and followers data

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+	Container,
+	Banner,
+	Avatar,
+	ProfileData,
+	LocationIcon,
+	CakeIcon,
+	Followage,
+} from './styles';
 
 const ProfilePage: React.FC = () => {
-	return <Container></Container>;
+	return (
+		<Container>
+			<Banner>
+				<Avatar />
+			</Banner>
+
+			<ProfileData>
+				{/* <EditButton outlined>Editar Perfil</EditButton> */}
+				<h1>Bruno Santi</h1>
+				<h2>@brsanti</h2>
+
+				<p>
+					Full Stack Web Developer at{' '}
+					<a href="https://operdata.com.br">@operdata</a>
+				</p>
+				<ul>
+					<li>
+						<LocationIcon />
+						Belo Horizontte, Brasil
+					</li>
+					<li>
+						<CakeIcon />
+						Nascido(a) em 2 de agosto de 1998
+					</li>
+				</ul>
+
+				<Followage>
+					<span>
+						Seguindo <strong>35</strong>
+					</span>
+					<span>
+						<strong>157 </strong> Seguidores
+					</span>
+				</Followage>
+			</ProfileData>
+		</Container>
+	);
 };
 
 export default ProfilePage;
