588754d10c715256024a4f77d04ce766de8e613c
Bruno Santi Garcia <bruno.santi.98@outlook.com>
2020-07-21 20:05:36 -0300

    [+] Add TopBar

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Main from '../Main';
 import { Container, Wrapper } from './styles';
+import MenuBar from '../MenuBar';
 
 const Layout: React.FC = () => {
 	return (
 		<Container>
 			<Wrapper>
-				{/* <MenuBar /> */}
+				<MenuBar />
 				<Main />
 				{/* <SideBar /> */}
 			</Wrapper>
